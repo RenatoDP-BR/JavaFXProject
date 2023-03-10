9365e10286931923ef371e517ab28e6690220ac9
Renato dias Pereira <renatodp@hotmail.com>
2023-03-10 16:01:08 -0300

    Foi criada uma pequena tela de simulação de um calculo matematico de soma

---
+++ src/test/java/com/example/helloworld/HelloApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloApplicationTest {
+
+    public void ValidarOperacoesDeSoma() {
+
+    }
+
+    public void ValidarOperacoesDeSubtracao() {
+
+    }
+}
